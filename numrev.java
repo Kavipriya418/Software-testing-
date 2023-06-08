1926e93ebacbe1f23c66e6a23ed979bffac42c84
Kavipriya418 <113329595+Kavipriya418@users.noreply.github.com>
2023-06-08 12:32:09 +0530

    Create numrev.java

---
+++ numrev.java
@@ -0,0 +1,61 @@
+#Open
+
+numrev.java
+
+#4
+
+Mounikachenna12 opened this issue 1 hour ago · 0 comments
+
+Comments
+
+Mounikachenna12 commented 1 hour ago
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.*;
+
+class numrev{
+
+public static void main(String []args){
+
+Scanner a=new Scanner(System.in);
+
+try{
+
+int num=a.nextInt();
+
+System.out.println(Numrev(num));
+
+assertTrue(4321==Numrev(num));
+
+}
+
+catch (Exception e){
+
+System.out.println("enter the integer number only");
+
+}
+
+a.close();
+
+}
+
+public static int Numrev(int num){
+
+int rev=0;
+
+while(num!=0){
+
+int rem=num%10;
+
+rev=rev*10+rem;
+
+num=num/10;
+
+}
+
+return rev;
+
+}
+
+}
